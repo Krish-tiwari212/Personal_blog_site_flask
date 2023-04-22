f6751db65f410be0d1146ddb35c0eeaa3c70b577
Krish Tiwari <114987949+Krish-tiwari212@users.noreply.github.com>
2023-04-22 15:56:59 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template,redirect, request
 import smtplib
 from email.mime.text import MIMEText
+from flask_sqlalchemy import SQLAlchemy
 import requests
+from flask_ckeditor import CKEditor
+from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditorField
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+import datetime
 
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body)
@@ -15,23 +23,41 @@ def send_email(subject, body, sender, recipients, password):
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYpEfBA6O6donzWlSihBXox7C0sKR6'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///posts.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+ckeditor = CKEditor(app)
+Bootstrap(app)
+db = SQLAlchemy()
+db.init_app(app)
+
+class Form(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    name = StringField("Your Name", validators=[DataRequired()])
+    url = StringField("Blog Image Url", validators=[DataRequired()])
+    content = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class blog_post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.VARCHAR(250), nullable=False, unique=True)
+    date = db.Column(db.VARCHAR(250),nullable=False)
+    body = db.Column(db.TEXT, nullable=False)
+    author = db.Column(db.VARCHAR(250), nullable=False)
+    img_url = db.Column(db.VARCHAR(250), nullable=False)
+    subtitle = db.Column(db.VARCHAR(250), nullable=False)
+with app.app_context():
+    c = blog_post.query.all()
+
 
 @app.route("/")
 def home():
-    c = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json")
-    c = c.json()
+    c = blog_post.query.all()
     return render_template("index.html",l=c)
-@app.route("/posts")
-def red_home():
-    c = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json")
-    c = c.json()
-    return redirect("/")
 @app.route("/about")
 def about():
     return render_template("about.html")
-@app.route("/posts/about")
-def red_about():
-    return redirect("/about")
 @app.route("/contact",methods=["GET","POST"])
 def contact():
     if request.method == "POST":
@@ -40,25 +66,62 @@ def contact():
         phone = request.form["phone"]
         mes = request.form["message"]
         h = "Successfully sent your message"
-        sub = "Someone Contacted you"
-        body = f"Hi\nName = {name}\nemail = {email}\nphone = {phone}\nmessage = {mes}"
-        sender_add = 'sender_email'
-        sender_pass = 'sender_pass'
-        receiver = ['receiver_mail']
+        sub = "Someone Contacted You"
+        body = f"Hi\nName = {name}\nEmail = {email}\nPhone = {phone}\nMessage = {mes}"
+        sender_add = 'champion.coc212@gmail.com'
+        sender_pass = 'xdtnxbfuishleozw'
+        receiver = ['champion.coc212@gmail.com']
         send_email(sub, body, sender_add, receiver, sender_pass)
         return render_template("contact.html", hed=h)
     else:
         h = "Contact Me"
         return render_template("contact.html", hed=h)
-@app.route("/posts/contact")
-def red_contact():
-    return redirect("/contact")
 @app.route("/posts/<num>")
 def pos(num):
-    c = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json")
-    c = c.json()
+    c = blog_post.query.all()
     return render_template("post.html", l=c,n=num)
 
+@app.route("/new-post", methods = ["GET","POST"])
+def new_post():
+    form = Form()
+    if request.method == "POST" and form.validate_on_submit():
+        name = form.name.data
+        title = form.title.data
+        subtitle = form.subtitle.data
+        img = form.url.data
+        cont = form.content.data
+        x = datetime.datetime.now()
+        blog = blog_post(title=title, date=f"{x.strftime('%B')} {x.strftime('%d')},{x.strftime('%Y')}", body=cont,
+                         author="Krish", img_url=img, subtitle=subtitle)
+        db.session.add(blog)
+        db.session.commit()
+        return redirect("/")
+    return render_template('new_post.html',form=form,c=0)
+@app.route("/edit-post/<num>", methods = ["GET","POST"])
+def edit(num):
+    c = db.session.query(blog_post).filter_by(id=int(num)+1).first()
+    form = Form()
+    if request.method == "GET":
+        form.title.data = c.title
+        form.subtitle.data = c.subtitle
+        form.url.data = c.img_url
+        form.content.data = c.body
+        form.name.data = c.author
+        return render_template("new_post.html",form=form,c=1)
+    if request.method == "POST" and form.validate_on_submit():
+        c.title = form.title.data
+        c.subtitle = form.subtitle.data
+        c.img_url = form.url.data
+        c.body = form.content.data
+        c.author = form.name.data
+        db.session.commit()
+        return redirect(f"/posts/{num}")
+@app.route("/delete/<num>")
+def dele(num):
+    c = db.session.query(blog_post).filter_by(id=int(num)).first()
+    db.session.delete(c)
+    db.session.commit()
+    return redirect("/")
 # @app.route("/login", methods=["GET","POST"])
 # def login():
 #     if request.method == "POST":
